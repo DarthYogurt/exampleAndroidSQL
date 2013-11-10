4e04f062df6938a66e4f9cc159b17177f6756a2e
Raymond Walintukan <raytochina@gmail.com>
2013-11-10 15:44:24 -0800

    Working version of SQL example

---
+++ src/com/example/de/vogella/android/sqlite/first/Comment.java
@@ -0,0 +1,31 @@
+package com.example.de.vogella.android.sqlite.first;
+
+public class Comment {
+	private long id;
+	private String comment;
+	
+	public long getId(){
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getcomment(){
+		return comment;
+	}
+	
+	public void setComment(String comment){
+		this.comment = comment;
+	}
+
+	// Will be used by the ArrayAdapter in the ListView
+	public String toString(){
+		return comment;
+	}
+	
+
+
+	
+}
